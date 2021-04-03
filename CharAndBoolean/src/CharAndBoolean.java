2e4eb36876792f40391a693b1cee61d353c75493
David-B-Comer <davec2233@gmail.com>
2021-04-03 17:18:05 -0400

    added myCopyrightChar

--- CharAndBoolean/src/CharAndBoolean.java
+++ CharAndBoolean/src/CharAndBoolean.java
@@ -6,5 +6,8 @@ public class CharAndBoolean {
         char myUnicodeChar = '\u0044';
         System.out.println(myChar);
         System.out.println(myUnicodeChar);
+
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
     }
 }
