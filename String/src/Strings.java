655eaedc51491191ddac63dbd60fcc006ce00920
David-B-Comer <davec2233@gmail.com>
2021-04-03 17:39:33 -0400

    added myString

---
+++ String/src/Strings.java
@@ -0,0 +1,8 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+    }
+}
