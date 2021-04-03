24315436adc7a5d3e1332cb67f136aaa079c9982
David-B-Comer <davec2233@gmail.com>
2021-04-02 20:46:39 -0400

    added int float and double update

--- FloatAndDouble/src/FloatAndDouble.java
+++ FloatAndDouble/src/FloatAndDouble.java
@@ -14,9 +14,13 @@ public class FloatAndDouble {
         System.out.println("Double maximum value + " + myMaxDoubleValue);
 
 
-        int myIntValue = 5;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
 
     }
 }
