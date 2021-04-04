fc8f2aae2e22ca42034bd90b3696f3ac133caa77
David-B-Comer <davec2233@gmail.com>
2021-04-04 08:05:43 -0400

    added previous result

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -4,5 +4,12 @@ public class Operators {
 
         int result = 1 + 2; // 1 + 2 = 3
         System.out.println("1 + 2 = " + result);
+
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1; // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
     }
 }
