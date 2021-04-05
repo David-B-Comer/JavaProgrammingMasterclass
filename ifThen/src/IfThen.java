d478077b8760f75625854f38540a806afde1fe1c
David-B-Comer <davec2233@gmail.com>
2021-04-05 15:54:10 -0400

    added if then isAlien code block

--- ifThen/src/IfThen.java
+++ ifThen/src/IfThen.java
@@ -3,8 +3,9 @@ public class IfThen {
     public static void main(String[] args) {
 
         boolean isAlien = false;
-        if (isAlien == true)
+        if (isAlien == false) {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
+        }
     }
 }
