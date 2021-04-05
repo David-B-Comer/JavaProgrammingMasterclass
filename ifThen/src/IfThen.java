29d7192b58d1cfad5830e9b01a185be04e8ebfe8
David-B-Comer <davec2233@gmail.com>
2021-04-05 15:35:29 -0400

    added if then

---
+++ ifThen/src/IfThen.java
@@ -0,0 +1,9 @@
+public class IfThen {
+
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == true)
+            System.out.println("It is not an alien!");
+    }
+}
