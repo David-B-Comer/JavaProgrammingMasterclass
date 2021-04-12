86b7d7c1bcfa9022c98d2886fd31c88348861cdf
David-B-Comer <davec2233@gmail.com>
2021-04-12 10:57:47 -0400

    added keywords and expressions highscore

--- keywordsAndExpressions/src/keywordsAndExpressions.java
+++ keywordsAndExpressions/src/keywordsAndExpressions.java
@@ -4,5 +4,10 @@ public class keywordsAndExpressions {
         // a mile is equal to 1.609344 kilometers
         double kilometers = (100 * 1.609344);
 
+        int highScore = 50;
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
     }
 }
