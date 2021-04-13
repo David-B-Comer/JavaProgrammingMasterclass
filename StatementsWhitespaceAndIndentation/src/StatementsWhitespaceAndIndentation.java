a1ae7d72ce67d1cabcc75a963aac9b735022b68b
David-B-Comer <davec2233@gmail.com>
2021-04-13 09:15:34 -0400

    added StatementsWhitespaceAndIndentation myVariable

--- StatementsWhitespaceAndIndentation/src/StatementsWhitespaceAndIndentation.java
+++ StatementsWhitespaceAndIndentation/src/StatementsWhitespaceAndIndentation.java
@@ -2,5 +2,10 @@ public class StatementsWhitespaceAndIndentation {
 
     public static void main(String[] args) {
 
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
     }
 }
