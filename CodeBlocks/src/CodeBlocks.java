63c3ee9c6149d23c801b274e1c1f763383fcc656
David-B-Comer <davec2233@gmail.com>
2021-04-15 15:45:51 -0400

    added method calculate score

--- CodeBlocks/src/CodeBlocks.java
+++ CodeBlocks/src/CodeBlocks.java
@@ -3,22 +3,11 @@ public class CodeBlocks {
     public static void main(String[] args) {
 
         boolean gameOver = true;
-        int score = 5000;
+        int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-//        if (score < 5000 && score > 1000) {
-//            System.out.println("Your score was less than 5000 but greater than 1000");
-//        } else if (score < 1000) {
-//            System.out.println("your score was less than 1000");
-//        } else {
-//            System.out.println("Got here");
-//        }
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore();
 
         score = 10000;
         levelCompleted = 8;
@@ -29,4 +18,19 @@ public class CodeBlocks {
             System.out.println("Your final score was " + finalScore);
         }
     }
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+    }
+
 }
