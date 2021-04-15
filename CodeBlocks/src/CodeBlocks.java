bf87b1c54d6a98c18344dc80182603737706533b
David-B-Comer <davec2233@gmail.com>
2021-04-15 16:03:12 -0400

    added method calculate score arguments

--- CodeBlocks/src/CodeBlocks.java
+++ CodeBlocks/src/CodeBlocks.java
@@ -2,9 +2,20 @@ public class CodeBlocks {
 
     public static void main(String[] args) {
 
-        calculateScore(true, 800, 5, 100);
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
 
-        calculateScore(true, 10000, 8, 200);
+
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
     }
 
@@ -13,7 +24,6 @@ public class CodeBlocks {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
             return finalScore;
         }
         return -1;
