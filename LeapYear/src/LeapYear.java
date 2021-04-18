3413aa2be7070dcf74cb3ba19a12c74126ed3e04
David-B-Comer <davec2233@gmail.com>
2021-04-18 18:31:13 -0400

    added  isLeapYear

--- LeapYear/src/LeapYear.java
+++ LeapYear/src/LeapYear.java
@@ -1,4 +1,16 @@
 public class LeapYear {
 
+    public static void main(String[] args) {
+        isLeapYear(-1600);
+        isLeapYear(1600);
+    }
 
+    public static boolean isLeapYear(int year) {
+        if ((year < 1) || (year > 9999)) {
+            return false;
+        } else if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) ) {
+            return true;
+        }
+        return false;
+    }
 }
