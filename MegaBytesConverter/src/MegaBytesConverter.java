4081a0cb4f260151f2087dc54214e74760d51b0a
David-B-Comer <davec2233@gmail.com>
2021-04-18 17:45:11 -0400

    added MegaBytesConverter printMegaBytesAndKiloBytes

--- MegaBytesConverter/src/MegaBytesConverter.java
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -1,4 +1,16 @@
 public class MegaBytesConverter {
 
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(0);
+    }
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
 
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int megabytes = kiloBytes / 1024;
+            int remainingKilobytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKilobytes + " KB" );
+        }
+    }
 }
