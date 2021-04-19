83166c0b244846c851c838bb417c2674d6429bd3
David-B-Comer <davec2233@gmail.com>
2021-04-19 17:28:02 -0400

    added DecimalComparator areEqualByThreeDecimalPlaces

--- DecimalComparator/src/DecimalComparator.java
+++ DecimalComparator/src/DecimalComparator.java
@@ -1,2 +1,17 @@
 public class DecimalComparator {
+
+    public static void main(String[] args) {
+        areEqualByThreeDecimalPlaces(-3.1756, -3.175);
+        areEqualByThreeDecimalPlaces(3.175, 3.176);
+    }
+    public static boolean areEqualByThreeDecimalPlaces(double value1, double value2) {
+
+        if ((int) (value1 * 1000) == (int) (value2 * 1000)) {
+            System.out.println("true");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
 }
