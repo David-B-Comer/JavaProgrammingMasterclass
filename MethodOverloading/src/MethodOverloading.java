99c40b6a2f69985da108ccd03de95435a5caea8f
David-B-Comer <davec2233@gmail.com>
2021-04-20 11:14:21 -0400

    added MethodOverloading calculateScore no parameter

--- MethodOverloading/src/MethodOverloading.java
+++ MethodOverloading/src/MethodOverloading.java
@@ -4,6 +4,7 @@ public class MethodOverloading {
         int newScore = calculateScore("Tim", 500);
         System.out.println("New score is " + newScore);
         calculateScore(75);
+        calculateScore();
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -14,5 +15,10 @@ public class MethodOverloading {
     public static int calculateScore(int score) {
         System.out.println("Unnamed player scored " + score + " points");
         return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name, no player score.");
+        return 0;
     }
 }
