3017d1ed8f3d6d16bd8c9a88ba13e6a98c5da447
David-B-Comer <davec2233@gmail.com>
2021-04-20 11:55:30 -0400

    added MethodOverloading calcFeetAndInchesToCentimeters

--- MethodOverloading/src/MethodOverloading.java
+++ MethodOverloading/src/MethodOverloading.java
@@ -5,6 +5,7 @@ public class MethodOverloading {
         System.out.println("New score is " + newScore);
         calculateScore(75);
         calculateScore();
+        calcFeetAndInchesToCentimeters(6, 0);
     }
 
     public static int calculateScore(String playerName, int score) {
@@ -20,5 +21,18 @@ public class MethodOverloading {
     public static int calculateScore() {
         System.out.println("No player name, no player score.");
         return 0;
+    }
+
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches) {
+
+        if ((feet < 0) || (inches < 0) || (inches > 12)) {
+            System.out.println("Invalid feet or inches parameters");
+            return -1;
+        } else {
+            double centimeters = (feet * 12) * 2.54;
+            centimeters += inches * 2.54;
+            System.out.println(feet + " feet, " + inches + " inches = " + centimeters + " cm");
+        return centimeters;
+        }
     }
 }
