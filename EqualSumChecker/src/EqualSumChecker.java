a24099ec42076da3db5215cb912bc5f0030a7f45
David-B-Comer <davec2233@gmail.com>
2021-04-19 20:10:47 -0400

    added EqualSumChecker hasEqualSum

--- EqualSumChecker/src/EqualSumChecker.java
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -1,2 +1,16 @@
 public class EqualSumChecker {
+
+    public static void main(String[] args) {
+        hasEqualSum(1, 1, 1);
+        hasEqualSum(1,1, 2);
+        hasEqualSum(1, -1, 0);
+    }
+
+    public static boolean hasEqualSum(int value1, int value2, int value3) {
+
+        if ((value1 + value2) == value3){
+            return true;
+        }
+        return false;
+    }
 }
