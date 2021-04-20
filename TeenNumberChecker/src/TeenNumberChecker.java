576e6eff7dc6f2f8e2d701010b925e37f0265d2b
David-B-Comer <davec2233@gmail.com>
2021-04-19 20:19:26 -0400

    added TeenNumberChecker hasTeen

--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,2 +1,10 @@
 public class TeenNumberChecker {
+
+    public static boolean hasTeen(int value1, int value2, int value3) {
+        if (isTeen(value1) || isTeen(value2) || isTeen(value3)) {
+            return true;
+        }
+        return false;
+    }
+
 }
