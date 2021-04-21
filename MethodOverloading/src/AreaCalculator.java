63980cec93622c7af18d373a3a29ee32f30d293c
David-B-Comer <davec2233@gmail.com>
2021-04-21 16:54:18 -0400

    added AreaCalculator area

--- MethodOverloading/src/AreaCalculator.java
+++ MethodOverloading/src/AreaCalculator.java
@@ -1,4 +1,13 @@
 public class AreaCalculator {
 
+    private static final double PI = Math.PI;
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1.0;
+        }
+        return radius * radius * PI;
+    }
+
 
 }
