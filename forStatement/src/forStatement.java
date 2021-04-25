fa7a30ffa0088b37deb17f3bd04898e8d5eabb42
David-B-Comer <davec2233@gmail.com>
2021-04-25 11:40:07 -0400

    added forStatement calculateInterest for loop decrement

--- forStatement/src/forStatement.java
+++ forStatement/src/forStatement.java
@@ -14,6 +14,11 @@ public class forStatement {
         for (int i = 2; i < 9; i++) {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
+
+        for (int i = 8; i > 1; i--) {
+            System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
+        }
+
     }
 
     public static double calculateInterest(double amount, double interestRate) {
