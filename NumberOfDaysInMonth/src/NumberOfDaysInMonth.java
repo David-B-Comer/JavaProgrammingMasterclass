51566e8e744ff0389e2e99caf7a927d6964b0c2c
David-B-Comer <davec2233@gmail.com>
2021-04-25 11:18:54 -0400

    added NumberOfDaysInMonth getDaysInMonth

--- NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
+++ NumberOfDaysInMonth/src/NumberOfDaysInMonth.java
@@ -9,6 +9,22 @@ public class NumberOfDaysInMonth {
         return false;
     }
 
-
+    public static int getDaysInMonth(int month, int year) {
+        if ((month < 1) || (month > 12)) {
+            return -1;
+        } else if ((year < 1) || (year > 9999)) {
+            return -1;
+        } else if (isLeapYear(year) && (month == 2)) {
+            return 29;
+        }
+        switch (month) {
+            case 2:
+                return 28;
+            case 4: case 6: case 9: case 11:
+                return 30;
+            default:
+                return 31;
+        }
+    }
 
 }
