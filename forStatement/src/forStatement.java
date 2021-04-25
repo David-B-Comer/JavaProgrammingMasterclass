19d0f3c5239b9b7d69129a43a7db88051fadb145
David-B-Comer <davec2233@gmail.com>
2021-04-25 11:46:23 -0400

    added forStatement isPrime

--- forStatement/src/forStatement.java
+++ forStatement/src/forStatement.java
@@ -15,10 +15,26 @@ public class forStatement {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
+        System.out.println("*******************************************");
+
         for (int i = 8; i > 1; i--) {
             System.out.println("10,000 at " + i + "% interest = " + String.format("%.2f", calculateInterest(10000.0, i)));
         }
 
+    }
+
+
+    public static boolean isPrime(int n) {
+
+        if (n == 1) {
+            return false;
+        }
+        for (int i = 2; i <= n/2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static double calculateInterest(double amount, double interestRate) {
