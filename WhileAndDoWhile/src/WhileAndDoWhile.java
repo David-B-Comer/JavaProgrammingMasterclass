17a040c414ff25a9d25d9a1301a29efad03a5960
David-B-Comer <davec2233@gmail.com>
2021-04-27 19:43:56 -0400

    added WhileAndDoWhile count while

--- WhileAndDoWhile/src/WhileAndDoWhile.java
+++ WhileAndDoWhile/src/WhileAndDoWhile.java
@@ -1,2 +1,13 @@
 public class WhileAndDoWhile {
+
+
+    public static void main(String[] args) {
+
+        int count = 1;
+
+        while (count != 6) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
 }
