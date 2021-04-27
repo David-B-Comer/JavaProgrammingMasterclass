505b5c2ba62351f65914d0e9c1169fb427502fb9
David-B-Comer <davec2233@gmail.com>
2021-04-26 22:09:21 -0400

    added SumOddRange sumOdd

--- SumOddRange/src/SumOddRange.java
+++ SumOddRange/src/SumOddRange.java
@@ -9,5 +9,17 @@ public class SumOddRange {
         return false;
     }
 
+    public static int sumOdd(int start, int end) {
 
+        if ((end < start) || (start < 0) || (end < 0)) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
 }
