a1531a64a6848bd902982f6f667249f92038fb92
David-B-Comer <davec2233@gmail.com>
2021-04-29 18:16:37 -0400

    added DigitSum sumDigits

--- DigitSum/src/DigitSum.java
+++ DigitSum/src/DigitSum.java
@@ -1,2 +1,18 @@
 public class DigitSum {
+
+
+    public static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
 }
