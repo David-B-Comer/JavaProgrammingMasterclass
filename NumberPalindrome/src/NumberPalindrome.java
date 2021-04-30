dc712a95e88ceed372fab720c1cd3cf56e0d2ca2
David-B-Comer <davec2233@gmail.com>
2021-04-30 13:04:21 -0400

    added NumberPalindrome isPalindrome

--- NumberPalindrome/src/NumberPalindrome.java
+++ NumberPalindrome/src/NumberPalindrome.java
@@ -1,2 +1,19 @@
 public class NumberPalindrome {
+
+    public static boolean isPalindrome(int number) {
+
+        int reverse = 0;
+        int num = number;
+
+        while (num != 0) {
+
+            reverse = (num % 10) + (reverse * 10);
+            num /= 10;
+        }
+        if (reverse == number) {
+            return true;
+        }
+        return false;
+    }
+
 }
