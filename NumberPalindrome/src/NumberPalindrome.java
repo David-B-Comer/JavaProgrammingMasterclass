892c6136accb2a9ccf2e50d61b9144e4c4009696
David-B-Comer <davec2233@gmail.com>
2021-04-30 13:06:45 -0400

    added NumberPalindrome isPalindrome method calls

--- NumberPalindrome/src/NumberPalindrome.java
+++ NumberPalindrome/src/NumberPalindrome.java
@@ -1,5 +1,13 @@
 public class NumberPalindrome {
 
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-1221)
+        );
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+    }
+
     public static boolean isPalindrome(int number) {
 
         int reverse = 0;
