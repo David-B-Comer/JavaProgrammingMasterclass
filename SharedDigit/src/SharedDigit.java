c19d5972363fe10477ee5cd1c08aaca9b986e609
David-B-Comer <davec2233@gmail.com>
2021-05-01 17:06:18 -0400

    added SharedDigit hasSharedDigit

--- SharedDigit/src/SharedDigit.java
+++ SharedDigit/src/SharedDigit.java
@@ -1,2 +1,12 @@
 public class SharedDigit {
+
+    public static boolean hasSharedDigit(int n1, int n2) {
+        if ((n1 < 10) || (n1 > 99) || (n2 < 10) || (n2 > 99)) {
+            return false;
+        }
+        else if ((n1 / 10 == n2 / 10) || (n1 / 10 == n2 % 10) || (n1 % 10 == n2 / 10) || (n1 % 10 == n2 % 10)) {
+            return true;
+        }
+        return false;
+    }
 }
