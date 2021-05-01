b5e09105238f2e863005d23fe8eb618ea31250c5
David-B-Comer <davec2233@gmail.com>
2021-05-01 16:49:37 -0400

    added EvenDigitSum getEvenDigitSum method calls

--- EvenDigitSum/src/EvenDigitSum.java
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -1,5 +1,11 @@
 public class EvenDigitSum {
 
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
     public static int getEvenDigitSum(int number) {
         if (number < 0) {
             return -1;
