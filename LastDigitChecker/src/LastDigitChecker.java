4d653b668580c479ac568332f17f05ce618a2a1a
David-B-Comer <davec2233@gmail.com>
2021-05-02 14:08:20 -0400

    added LastDigitChecker hasSameLastDigit

--- LastDigitChecker/src/LastDigitChecker.java
+++ LastDigitChecker/src/LastDigitChecker.java
@@ -1,5 +1,19 @@
 public class LastDigitChecker {
 
+    public static boolean hasSameLastDigit(int n1, int n2, int n3) {
+
+        if (!isValid(n1) || !isValid(n2) || !isValid(n3)) {
+            return false;
+        }
+        int rightDigit1 = n1 % 10;
+        int rightDigit2 = n2 % 10;
+        int rightDigit3 = n3 % 10;
+
+        if ((rightDigit1 == rightDigit2) || (rightDigit1 == rightDigit3) || (rightDigit2 == rightDigit3)) {
+            return true;
+        }
+        return false;
+    }
 
 
     public static boolean isValid(int number) {
