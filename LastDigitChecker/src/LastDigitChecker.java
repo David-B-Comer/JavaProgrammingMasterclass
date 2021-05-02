b4b7c5021374078b6754b1752e2ad4d82ade455e
David-B-Comer <davec2233@gmail.com>
2021-05-02 14:10:56 -0400

    added LastDigitChecker hasSameLastDigit method calls

--- LastDigitChecker/src/LastDigitChecker.java
+++ LastDigitChecker/src/LastDigitChecker.java
@@ -1,5 +1,15 @@
 public class LastDigitChecker {
 
+    public static void main(String[] args) {
+
+        System.out.println(hasSameLastDigit(41, 22, 71));
+        System.out.println(hasSameLastDigit(23, 32, 42));
+        System.out.println(hasSameLastDigit(9, 99, 999));
+        System.out.println(isValid(10));
+        System.out.println(isValid(468));
+        System.out.println(isValid(1051));
+    }
+
     public static boolean hasSameLastDigit(int n1, int n2, int n3) {
 
         if (!isValid(n1) || !isValid(n2) || !isValid(n3)) {
