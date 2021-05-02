8f22f782b659c68b864ad716e3a46678ae530430
David-B-Comer <davec2233@gmail.com>
2021-05-02 14:26:07 -0400

    added GreatestCommonDivisor getGreatestCommonDivisor method calls

--- GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -1,5 +1,13 @@
 public class GreatestCommonDivisor {
 
+    public static void main(String[] args) {
+
+        System.out.println(getGreatestCommonDivisor(25, 15));
+        System.out.println(getGreatestCommonDivisor(12, 30));
+        System.out.println(getGreatestCommonDivisor(9, 18));
+        System.out.println(getGreatestCommonDivisor(81, 153));
+    }
+
     public static int getGreatestCommonDivisor(int first, int second) {
 
         if ((first < 10) || (second < 10)) {
