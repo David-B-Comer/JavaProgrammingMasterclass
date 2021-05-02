ffdda3a7e1fcb666dd4b04dd2332730de1a6ae51
David-B-Comer <davec2233@gmail.com>
2021-05-02 14:07:45 -0400

    added LastDigitChecker isValid

--- LastDigitChecker/src/LastDigitChecker.java
+++ LastDigitChecker/src/LastDigitChecker.java
@@ -1,4 +1,11 @@
 public class LastDigitChecker {
 
 
+
+    public static boolean isValid(int number) {
+        if ((number >= 10) && (number <= 1000)) {
+            return true;
+        }
+        return false;
+    }
 }
