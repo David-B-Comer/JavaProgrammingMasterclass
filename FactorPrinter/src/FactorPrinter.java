172663000ae056889665922fc94697d320b7681d
David-B-Comer <davec2233@gmail.com>
2021-05-03 13:43:04 -0400

    added FactorPrinter printFactors method calls

--- FactorPrinter/src/FactorPrinter.java
+++ FactorPrinter/src/FactorPrinter.java
@@ -1,5 +1,12 @@
 public class FactorPrinter {
 
+    public static void main(String[] args) {
+
+        printFactors(6);
+        printFactors(32);
+        printFactors(10);
+        printFactors(-1);
+    }
 
     public static void printFactors(int number) {
 
