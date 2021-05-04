e137c148efc2c82414899491a014b8528b0f60cd
David-B-Comer <davec2233@gmail.com>
2021-05-04 11:26:03 -0400

    added NumberToWords getDigitCount

--- NumberToWords/src/NumberToWords.java
+++ NumberToWords/src/NumberToWords.java
@@ -1,3 +1,18 @@
 public class NumberToWords {
 
+    public static int getDigitCount(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        if (number == 0) {
+            return 1;
+        }
+        int digitCount = 0;
+
+        for (int i = 0; number > 0; i++) {
+            number /= 10;
+            digitCount++;
+        }
+        return digitCount;
+    }
 }
