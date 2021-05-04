f236ad882ff0e171b71efaa61da51c3a879d25d1
David-B-Comer <davec2233@gmail.com>
2021-05-04 11:27:22 -0400

    added NumberToWords numberToWords

--- NumberToWords/src/NumberToWords.java
+++ NumberToWords/src/NumberToWords.java
@@ -1,5 +1,60 @@
 public class NumberToWords {
 
+
+    public static void numberToWords(int number) {
+
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+
+        int reverseNumber = reverse(number);
+        String word = "";
+
+        for (int i = 0; i < getDigitCount(number); i++) {
+
+
+            switch (reverseNumber % 10) {
+
+                case 0:
+                    word += "Zero ";
+                    break;
+                case 1:
+                    word += "One ";
+                    break;
+                case 2:
+                    word += "Two ";
+                    break;
+                case 3:
+                    word += "Three ";
+                    break;
+                case 4:
+                    word += "Four ";
+                    break;
+                case 5:
+                    word += "Five ";
+                    break;
+                case 6:
+                    word += "Six ";
+                    break;
+                case 7:
+                    word += "Seven ";
+                    break;
+                case 8:
+                    word += "Eight ";
+                    break;
+                case 9:
+                    word += "Nine ";
+                    break;
+                default:
+                    word += "Invalid Value";
+                    break;
+            }
+            reverseNumber /= 10;
+        }
+        System.out.println(word);
+    }
+
+
     public static int reverse(int number) {
 
         int reverseNumber = 0;
