6e9c37ebdc8cc9404d5ec97dac472d4a1e339b2e
David-B-Comer <davec2233@gmail.com>
2021-05-05 11:38:19 -0400

    added ParsingValueFromString parseDouble

--- ParsingValueFromString/src/ParsingValueFromString.java
+++ ParsingValueFromString/src/ParsingValueFromString.java
@@ -2,10 +2,10 @@ public class ParsingValueFromString {
 
     public static void main(String[] args) {
 
-        String numberAsString = "2018";
+        String numberAsString = "2018.125";
         System.out.println("numberAsString = " + numberAsString);
 
-        int number = Integer.parseInt(numberAsString);
+        double number = Double.parseDouble(numberAsString);
         System.out.println("number = " + number);
 
 
