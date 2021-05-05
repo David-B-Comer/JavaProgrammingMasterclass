46e9d540358fb3e69407e192b5d82b45d874dcc3
David-B-Comer <davec2233@gmail.com>
2021-05-05 12:20:41 -0400

    added LargestPrime getLargestPrime

--- LargestPrime/src/LargestPrime.java
+++ LargestPrime/src/LargestPrime.java
@@ -1,2 +1,16 @@
 public class LargestPrime {
+
+    public static int getLargestPrime(int number) {
+
+        if (number < 2) {
+            return -1;
+        }
+
+        for (int i = number / 2; i >= 2; i--) {
+            if ((number % i) == 0) {
+                number = i;
+            }
+        }
+        return number;
+    }
 }
