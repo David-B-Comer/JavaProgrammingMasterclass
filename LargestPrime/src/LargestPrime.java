6d0c3b70d91e2a8768baf3405f348da5b74ce9b9
David-B-Comer <davec2233@gmail.com>
2021-05-05 12:21:44 -0400

    added LargestPrime getLargestPrime method calls

--- LargestPrime/src/LargestPrime.java
+++ LargestPrime/src/LargestPrime.java
@@ -1,5 +1,13 @@
 public class LargestPrime {
 
+    public static void main(String[] args) {
+
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(-1));
+    }
+
     public static int getLargestPrime(int number) {
 
         if (number < 2) {
