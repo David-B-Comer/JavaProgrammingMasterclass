e1873d647a26aef05609b632195e36f4548f6288
David-B-Comer <davec2233@gmail.com>
2021-05-06 18:36:18 -0400

    added ReadingUserInput validation

--- ReadingUserInput/src/ReadingUserInput.java
+++ ReadingUserInput/src/ReadingUserInput.java
@@ -7,16 +7,26 @@ public class ReadingUserInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth:");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        boolean hasNextInt = scanner.hasNextInt();
 
-        int age = 2021 - yearOfBirth;
+        if (hasNextInt) {
 
-        if (age >= 0 && age <= 100) {
-            System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+
+            int age = 2021 - yearOfBirth;
+
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
         }
 
         scanner.close();
