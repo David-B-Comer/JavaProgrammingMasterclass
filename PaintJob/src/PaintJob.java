1f099a90c877d5cc618f920544d04b45d299b792
David-B-Comer <davec2233@gmail.com>
2021-05-09 14:34:37 -0400

    added PaintJob getBucketCount

--- PaintJob/src/PaintJob.java
+++ PaintJob/src/PaintJob.java
@@ -1,2 +1,16 @@
 public class PaintJob {
+
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double area = height * width;
+
+        return (int)(Math.ceil(area / areaPerBucket) - extraBuckets);
+    }
+
+
 }
