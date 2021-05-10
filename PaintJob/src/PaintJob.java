26babf367175963f625c6578800a7da0873eca9a
David-B-Comer <davec2233@gmail.com>
2021-05-10 09:54:50 -0400

    added PaintJob getBucketCount method calls

--- PaintJob/src/PaintJob.java
+++ PaintJob/src/PaintJob.java
@@ -1,5 +1,19 @@
 public class PaintJob {
 
+    public static void main(String[] args) {
+
+        getBucketCount(-3.4, 2.1, 1.5, 2);
+        getBucketCount(3.4, 2.1, 1.5, 2);
+        getBucketCount(2.75, 3.25, 2.5, 1);
+
+        getBucketCount(-3.4, 2.1, 1.5);
+        getBucketCount(3.4, 2.1, 1.5);
+        getBucketCount(7.25, 4.3, 2.35);
+
+        getBucketCount(3.4, 1.5);
+        getBucketCount(6.26, 2.2);
+        getBucketCount(3.26, 0.75);
+    }
 
     public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
 
