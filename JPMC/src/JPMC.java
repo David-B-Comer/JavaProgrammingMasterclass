8cdfee4a397128864b515ecd6a38ef5870bdd3af
David-B-Comer <davec2233@gmail.com>
2021-05-11 18:40:01 -0400

    added jpmc isDuplicate

--- JPMC/src/JPMC.java
+++ JPMC/src/JPMC.java
@@ -1,4 +1,26 @@
+import java.util.Arrays;
+
 public class JPMC {
+
+    // list of integers
+    // check if duplicate number exists
+
+
+    public static boolean isDuplicate(int[] numbers) {
+
+        if (numbers == null) {
+            return false;
+        }
+
+        Arrays.sort(numbers);
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == (numbers[i + 1])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 
