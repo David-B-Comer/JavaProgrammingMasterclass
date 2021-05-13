845bc30ca0ee4aae7042be1e8b656166c5aac719
David-B-Comer <davec2233@gmail.com>
2021-05-13 11:14:29 -0400

    added SimpleCalculator method calls

--- SimpleCalculator/src/SimpleCalculator.java
+++ SimpleCalculator/src/SimpleCalculator.java
@@ -1,5 +1,19 @@
 public class SimpleCalculator {
 
+
+    public static void main(String[] args) {
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtract= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("divide= " + calculator.getDivisionResult());
+
+    }
+
     private double firstNumber;
     private double secondNumber;
 
