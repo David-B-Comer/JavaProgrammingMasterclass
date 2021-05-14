787a272b47c47e1fa883eca703f63365043eccd9
David-B-Comer <davec2233@gmail.com>
2021-05-14 10:15:22 -0400

    added Person method calls

--- Person/src/Person.java
+++ Person/src/Person.java
@@ -1,5 +1,22 @@
 public class Person {
 
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setFirstName("");
+        person.setLastName("");
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");
+        System.out.println("fullName= " + person.getFullName());
+    }
+
     private String firstName;
     private String lastName;
     private int age;
