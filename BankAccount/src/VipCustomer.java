3cda7c2b372ff0a3390c6060282a916ebba4eecb
David-B-Comer <davec2233@gmail.com>
2021-05-16 09:48:35 -0400

    added VipCustomer overloaded constructor

--- BankAccount/src/VipCustomer.java
+++ BankAccount/src/VipCustomer.java
@@ -4,6 +4,14 @@ public class VipCustomer {
     private double creditLimit;
     private String email;
 
+    public VipCustomer() {
+        this("New Customer", 50000.00, "newcustomer@email.com");
+    }
+
+    public VipCustomer(String name, double creditLimit) {
+        this(name, creditLimit, "default@email.com");
+    }
+
     public VipCustomer(String name, double creditLimit, String email) {
         this.name = name;
         this.creditLimit = creditLimit;
