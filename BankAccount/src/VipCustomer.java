c67ec95a914edf556d12b1aa88bb1aea79341179
David-B-Comer <davec2233@gmail.com>
2021-05-16 09:40:44 -0400

    added VipCustomer constructor

--- BankAccount/src/VipCustomer.java
+++ BankAccount/src/VipCustomer.java
@@ -1,2 +1,12 @@
 public class VipCustomer {
+
+    private String name;
+    private double creditLimit;
+    private String email;
+
+    public VipCustomer(String name, double creditLimit, String email) {
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.email = email;
+    }
 }
