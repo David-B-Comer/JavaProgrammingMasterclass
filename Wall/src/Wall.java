16eeb23db297cdf4bdc717d1564ac09cdf6084c8
David-B-Comer <davec2233@gmail.com>
2021-05-16 10:17:59 -0400

    added Wall setHeight

--- Wall/src/Wall.java
+++ Wall/src/Wall.java
@@ -36,5 +36,13 @@ public class Wall {
         return height;
     }
 
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+        } else {
+            this.height = height;
+        }
+    }
+
 
 }
