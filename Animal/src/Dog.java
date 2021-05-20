34e7bd513d0a40d4676b4779b736f73a05e504de
David-B-Comer <davec2233@gmail.com>
2021-05-20 09:02:44 -0400

    added Animal Dog constructor

--- Animal/src/Dog.java
+++ Animal/src/Dog.java
@@ -1,6 +1,17 @@
 public class Dog extends Animal{
 
-    public Dog(String name, int brain, int body, int size, int weight) {
-        super(name, brain, body, size, weight);
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.legs = legs;
+        this. tail = tail;
+        this. teeth = teeth;
+        this.coat = coat;
     }
 }
