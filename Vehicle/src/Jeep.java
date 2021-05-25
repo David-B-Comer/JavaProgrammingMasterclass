8d934bf14ccbfb26b80a5939d2fbc20a8cb23658
David-B-Comer <davec2233@gmail.com>
2021-05-24 20:09:16 -0400

    added Vehicle Jeep constructor

--- Vehicle/src/Jeep.java
+++ Vehicle/src/Jeep.java
@@ -1,2 +1,9 @@
 public class Jeep extends Car1{
+
+    private int roadServiceMonths;
+
+    public Jeep(int roadServiceMonths) {
+        super("Jeep", "4WD", 4, 5, 5, false);
+        this.roadServiceMonths = roadServiceMonths;
+    }
 }
