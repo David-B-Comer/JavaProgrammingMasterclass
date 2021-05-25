ae1f2dbfdfe1885f2e9fcb70097cd16c269aa8c8
David-B-Comer <davec2233@gmail.com>
2021-05-24 20:35:11 -0400

    added VehicleMain method calls

--- Vehicle/src/VehicleMain.java
+++ Vehicle/src/VehicleMain.java
@@ -1,2 +1,10 @@
 public class VehicleMain {
+
+    public static void main(String[] args) {
+        Jeep jeep = new Jeep(36);
+        jeep.steer(45);
+        jeep.accelerate(30);
+        jeep.accelerate(20);
+        jeep.accelerate(-42);
+    }
 }
