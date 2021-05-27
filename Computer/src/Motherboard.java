cd98adf23b5821a97b549c6e17f85463169b5014
David-B-Comer <davec2233@gmail.com>
2021-05-27 09:20:59 -0400

    added Computer Motherboard constructor

--- Computer/src/Motherboard.java
+++ Computer/src/Motherboard.java
@@ -1,2 +1,16 @@
 public class Motherboard {
+
+    private String model;
+    public String manufacturer;
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+
+    public Motherboard(String model, String manufacturer, int ramSlots, int cardSlots, String bios) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.ramSlots = ramSlots;
+        this.cardSlots = cardSlots;
+        this.bios = bios;
+    }
 }
