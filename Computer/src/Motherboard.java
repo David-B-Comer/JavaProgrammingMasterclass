b685b5a4afd1f4869705415430f125fb5036be62
David-B-Comer <davec2233@gmail.com>
2021-05-27 09:27:29 -0400

    added Computer Motherboard loadProgram

--- Computer/src/Motherboard.java
+++ Computer/src/Motherboard.java
@@ -14,6 +14,10 @@ public class Motherboard {
         this.bios = bios;
     }
 
+    public void loadProgram(String programName) {
+        System.out.println("Program " + programName + " is now loading...");
+    }
+
     public String getModel() {
         return model;
     }
