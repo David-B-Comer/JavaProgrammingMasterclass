cf5afc9d67e773ad1184c1f57e7cb47f7ff34aae
David-B-Comer <davec2233@gmail.com>
2021-05-28 15:08:04 -0400

    added Computer PC constructor

--- Computer/src/PC.java
+++ Computer/src/PC.java
@@ -1,2 +1,13 @@
 public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
 }
