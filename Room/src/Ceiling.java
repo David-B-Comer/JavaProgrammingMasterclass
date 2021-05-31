e386c50c1102b5ab83d5919678ff9929878dcf98
David-B-Comer <davec2233@gmail.com>
2021-05-31 15:08:15 -0400

    added Room Ceiling constructor

--- Room/src/Ceiling.java
+++ Room/src/Ceiling.java
@@ -1,2 +1,10 @@
 public class Ceiling {
+
+    private int height;
+    private int paintedColor;
+
+    public Ceiling(int height, int paintedColor) {
+        this.height = height;
+        this.paintedColor = paintedColor;
+    }
 }
