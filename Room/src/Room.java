4466498693b2c947f72317775995588a1e609fc8
David-B-Comer <davec2233@gmail.com>
2021-05-30 21:04:33 -0400

    added Room constructor

--- Room/src/Room.java
+++ Room/src/Room.java
@@ -1,2 +1,22 @@
 public class Room {
+
+    private String name;
+    private Wall wall1;
+    private Wall wall2;
+    private Wall wall3;
+    private Wall wall4;
+    private Ceiling ceiling;
+    private Desk desk;
+    private Light light;
+
+    public Room(String name, Wall wall1, Wall wall2, Wall wall3, Wall wall4, Ceiling ceiling, Desk desk, Light light) {
+        this.name = name;
+        this.wall1 = wall1;
+        this.wall2 = wall2;
+        this.wall3 = wall3;
+        this.wall4 = wall4;
+        this.ceiling = ceiling;
+        this.desk = desk;
+        this.light = light;
+    }
 }
