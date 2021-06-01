f6ced3812e13e7943f9bdfe33b56ddd0b240a922
David-B-Comer <davec2233@gmail.com>
2021-06-01 09:37:10 -0400

    added Room Light constructor

--- Room/src/Light.java
+++ Room/src/Light.java
@@ -1,2 +1,12 @@
 public class Light {
+
+    private String style;
+    private boolean isOn;
+    private int globes;
+
+    public Light(String style, boolean isOn, int globes) {
+        this.style = style;
+        this.isOn = isOn;
+        this.globes = globes;
+    }
 }
