7de8387261723b9f9415fb12114c8667267a6410
David-B-Comer <davec2233@gmail.com>
2021-06-02 12:41:42 -0400

    added Printer PrinterMain method calls

--- Printer/src/PrinterMain.java
+++ Printer/src/PrinterMain.java
@@ -1,2 +1,13 @@
 public class PrinterMain {
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(50, false);
+        System.out.println("initial page count = " + printer.getNumberOfPagesPrinted());
+        int numberOfPagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + numberOfPagesPrinted + " new total print count for printer = " + printer.getNumberOfPagesPrinted());
+        numberOfPagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + numberOfPagesPrinted + " new total print count for printer = " + printer.getNumberOfPagesPrinted());
+
+    }
 }
