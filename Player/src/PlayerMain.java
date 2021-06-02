5c4e581cc2964a7fa6e0944c3a8fc82ca924cd9c
David-B-Comer <davec2233@gmail.com>
2021-06-02 11:35:55 -0400

    added Player PlayerMain method calls

--- Player/src/PlayerMain.java
+++ Player/src/PlayerMain.java
@@ -1,2 +1,19 @@
 public class PlayerMain {
+
+    public static void main(String[] args) {
+
+        Player player = new Player();
+        player.name = "Dave";
+        player.health = 20;
+        player.weapon = "Sword";
+
+        int damage = 10;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+
+        damage = 11;
+        player.loseHealth(damage);
+        System.out.println("Remaining health = " + player.healthRemaining());
+    }
+
 }
