436ed84df00d30f69dc5943e15c5b428d6be2be4
David-B-Comer <davec2233@gmail.com>
2021-06-02 12:25:19 -0400

    added Printer printPages

--- Printer/src/Printer.java
+++ Printer/src/Printer.java
@@ -28,4 +28,18 @@ public class Printer {
         }
     }
 
+    public int printPages(int pages) {
+        int pagesToPrint = pages;
+        if (this.isDuplexPrinter) {
+            pagesToPrint = (pages / 2) + (pages % 2);
+            System.out.println("Printing in duplex mode");
+        }
+
+        this.numberOfPagesPrinted += pagesToPrint;
+
+        return pagesToPrint;
+    }
+
+
+
 }
