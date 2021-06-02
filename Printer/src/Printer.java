5e77b9bb2512693e666890c2bea03ab19336aef3
David-B-Comer <davec2233@gmail.com>
2021-06-02 12:19:02 -0400

    added Printer constructor

--- Printer/src/Printer.java
+++ Printer/src/Printer.java
@@ -1,2 +1,20 @@
 public class Printer {
+
+    private int tonerLevel;
+    private int numberOfPagesPrinted;
+    private boolean isDuplexPrinter;
+
+    public Printer(int tonerLevel, boolean isDuplexPrinter) {
+
+        if(tonerLevel > -1 && tonerLevel <= 100) {
+            this.tonerLevel = tonerLevel;
+        } else {
+            this.tonerLevel = -1;
+        }
+
+        this.isDuplexPrinter = isDuplexPrinter;
+        this.numberOfPagesPrinted = 0;
+    }
+
+
 }
