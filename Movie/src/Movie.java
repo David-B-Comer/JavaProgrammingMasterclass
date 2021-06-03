9dc5430af3787a22da95b17e2b0159f3eddb44cc
David-B-Comer <davec2233@gmail.com>
2021-06-03 10:54:30 -0400

    added Movie IndependenceDay plot

--- Movie/src/Movie.java
+++ Movie/src/Movie.java
@@ -27,4 +27,9 @@ class IndependenceDay extends Movie {
     public IndependenceDay() {
         super("Independence Day");
     }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
 }
