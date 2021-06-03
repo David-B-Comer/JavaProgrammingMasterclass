318217b43000a4f9bbe46e4867ff9339a3480e4f
David-B-Comer <davec2233@gmail.com>
2021-06-03 11:00:13 -0400

    added Movie StarWars plot

--- Movie/src/Movie.java
+++ Movie/src/Movie.java
@@ -51,4 +51,9 @@ class StarWars extends Movie {
     public StarWars() {
         super("Star Wars");
     }
+
+    @Override
+    public String plot() {
+        return "Imperial Forces try to take over the universe";
+    }
 }
