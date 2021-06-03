bf4632dc878a333554cddca189b20b87fb0ab9a6
David-B-Comer <davec2233@gmail.com>
2021-06-03 11:11:56 -0400

    added Movie randomMovie

--- Movie/src/Movie.java
+++ Movie/src/Movie.java
@@ -66,3 +66,34 @@ class Forgettable extends Movie {
 
     // No plot method
 }
+
+
+
+class MovieMain {
+
+    public static void main(String[] args) {
+
+    }
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was: " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+
+            case 2:
+                return new IndependenceDay();
+
+            case 3:
+                return new MazeRunner();
+
+            case 4:
+                return new StarWars();
+
+            case 5:
+                return new Forgettable();
+        }
+        return null;
+    }
+
+}
