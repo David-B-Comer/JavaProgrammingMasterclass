7c3ac86c794cfa262b24398b42047e66200135be
David-B-Comer <davec2233@gmail.com>
2021-06-04 16:37:55 -0400

    added Car constructor

--- Car/src/Car.java
+++ Car/src/Car.java
@@ -1,2 +1,14 @@
 public class Car {
+
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
 }
