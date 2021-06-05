29e61a30a59475c89c49c4ae480b09136895b4fd
David-B-Comer <davec2233@gmail.com>
2021-06-05 14:58:20 -0400

    added Car Mitsubishi startEngine

--- Car/src/Car.java
+++ Car/src/Car.java
@@ -41,4 +41,9 @@ class Mitsubishi extends Car {
     public Mitsubishi(int cylinders, String name) {
         super(cylinders, name);
     }
+
+    @Override
+    public String startEngine() {
+        return "Mitsubishi -> startEngine()";
+    }
 }
