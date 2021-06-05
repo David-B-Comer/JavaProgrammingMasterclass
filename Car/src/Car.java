f487badb503ab74b42a5d2ccf4ae92fe903210ae
David-B-Comer <davec2233@gmail.com>
2021-06-05 15:14:19 -0400

    added Car Ford startEngine

--- Car/src/Car.java
+++ Car/src/Car.java
@@ -66,6 +66,11 @@ class Ford extends Car {
     public Ford(int cylinders, String name) {
         super(cylinders, name);
     }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
 }
 
 
