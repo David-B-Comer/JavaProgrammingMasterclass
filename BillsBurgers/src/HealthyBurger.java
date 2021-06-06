7a90204c99d1e24ebc7610e41f9ffc8b504f16d2
David-B-Comer <davec2233@gmail.com>
2021-06-06 18:21:44 -0400

    added HealthyBurger constructor

--- BillsBurgers/src/HealthyBurger.java
+++ BillsBurgers/src/HealthyBurger.java
@@ -1,2 +1,6 @@
-public class HealthyBurger {
+public class HealthyBurger extends Hamburger {
+
+    public HealthyBurger(String meat, double price) {
+        super("Healthy Burger", meat, price, "Brown rye roll");
+    }
 }
