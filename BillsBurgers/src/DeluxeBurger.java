717f6d2403128800241d5445fcff401105ab8e8c
David-B-Comer <davec2233@gmail.com>
2021-06-06 18:13:44 -0400

    added DeluxeBurger addHamburgerAddition4

--- BillsBurgers/src/DeluxeBurger.java
+++ BillsBurgers/src/DeluxeBurger.java
@@ -20,4 +20,10 @@ public class DeluxeBurger extends Hamburger {
     public void addHamburgerAddition3(String name, double price) {
         System.out.println("Cannot add additional items to a deluxe burger");
     }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Cannot add additional items to a deluxe burger");
+    }
+
 }
