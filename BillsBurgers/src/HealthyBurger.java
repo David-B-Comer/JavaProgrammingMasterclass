ac530b1369a434b2d5c8524389ffb1952f1e8a2a
David-B-Comer <davec2233@gmail.com>
2021-06-06 18:27:00 -0400

    added HealthyBurger addHealthyAddition1

--- BillsBurgers/src/HealthyBurger.java
+++ BillsBurgers/src/HealthyBurger.java
@@ -1,6 +1,21 @@
 public class HealthyBurger extends Hamburger {
 
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+
     public HealthyBurger(String meat, double price) {
         super("Healthy Burger", meat, price, "Brown rye roll");
+    }
+
+    public void addHealthyAddition1() {
+
+        this.healthyExtra1Name = healthyExtra1Name;
+        this.healthyExtra2Name = healthyExtra2Name;
+
+        System.out.println("Added " + healthyExtra1Name + " for an extra " + healthyExtra1Price);
+
     }
 }
