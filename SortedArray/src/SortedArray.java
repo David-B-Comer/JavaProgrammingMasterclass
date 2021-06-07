67c3e2003fa6301aa519ad66b32b0ca9707ee75b
David-B-Comer <davec2233@gmail.com>
2021-06-07 19:08:33 -0400

    added getIntegers

--- SortedArray/src/SortedArray.java
+++ SortedArray/src/SortedArray.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class SortedArray {
+
+    public static int[] getIntegers(int number) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] intArray = new int[number];
+        System.out.println("Enter " + number + " integers:");
+
+        for (int i = 0; i < number; i++) {
+            intArray[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        return intArray;
+    }
 }
