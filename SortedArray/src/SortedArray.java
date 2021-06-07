fc2b305770755bc6601082fcacf58a85060bc492
David-B-Comer <davec2233@gmail.com>
2021-06-07 19:51:47 -0400

    added sortIntegers

--- SortedArray/src/SortedArray.java
+++ SortedArray/src/SortedArray.java
@@ -23,4 +23,20 @@ public class SortedArray {
             System.out.println("Element " + i + " contents " + intArray[i]);
         }
     }
+
+    public static int[] sortIntegers(int[] intArray) {
+
+    for (int i = 0; i < intArray.length; i++) {
+        for (int j = i + 1; j < intArray.length; j++) {
+
+            if (intArray[i] < intArray[j]) {
+                int temp = intArray[i];
+                intArray[i] = intArray[j];
+                intArray[j] = temp;
+            }
+        }
+    }
+    return intArray;
+    }
+
 }
