bef5c8ae5e764b6b40777a1bd0230931f9f1ada5
David-B-Comer <davec2233@gmail.com>
2021-06-08 13:27:26 -0400

    added MinimumElement readInteger

--- MinimumElement/src/MinimumElement.java
+++ MinimumElement/src/MinimumElement.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class MinimumElement {
+
+    private static int readInteger() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int elements = scanner.nextInt();
+        scanner.close();
+
+        return elements;
+    }
+
 }
