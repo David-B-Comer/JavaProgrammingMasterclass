f49f23c4a6cabb52b6f5b7a939dea84de309a497
David-B-Comer <davec2233@gmail.com>
2021-06-10 14:11:54 -0400

    added GroceryMain processArrayList copy

--- GroceryList/src/GroceryMain.java
+++ GroceryList/src/GroceryMain.java
@@ -54,7 +54,9 @@ public class GroceryMain {
         System.out.println("\t 3 - To modify an item in the list.");
         System.out.println("\t 4 - To remove an item from the list.");
         System.out.println("\t 5 - To search for an item in the list.");
-        System.out.println("\t 6 - To quit the application.");
+        System.out.println("\t 6 - To copy the grocery list.");
+        System.out.println("\t 7 - To quit the application.");
+
     }
 
     public static void addItem() {
