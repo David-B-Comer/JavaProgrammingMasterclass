fed7cc27c9816c2c5722f0758a900f32577a5721
David-B-Comer <davec2233@gmail.com>
2021-06-11 12:21:28 -0400

    added MobilePhone Contact createContact

--- MobilePhone/src/Contact.java
+++ MobilePhone/src/Contact.java
@@ -15,4 +15,9 @@ public class Contact {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    public static Contact createContact(String name, String phoneNumber) {
+
+        return new Contact(name, phoneNumber);
+    }
 }
