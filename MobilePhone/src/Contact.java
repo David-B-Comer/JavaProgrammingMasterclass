a3addcfc96b24c07640c52dea08a444c0aaf8718
David-B-Comer <davec2233@gmail.com>
2021-06-11 12:17:33 -0400

    added MobilePhone Contact constructor

--- MobilePhone/src/Contact.java
+++ MobilePhone/src/Contact.java
@@ -1,2 +1,11 @@
 public class Contact {
+
+    private String name;
+    private String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
 }
