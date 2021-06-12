628a4a3936a4bc1c2f48fdaa27c7ffcaf579b3e6
David-B-Comer <davec2233@gmail.com>
2021-06-12 09:24:11 -0400

    added MobilePhoneMain method calls

--- MobilePhone/src/MobilePhoneMain.java
+++ MobilePhone/src/MobilePhoneMain.java
@@ -1,2 +1,19 @@
 public class MobilePhoneMain {
+
+    public static void main(String[] args) {
+
+        MobilePhone mobilePhone = new MobilePhone("123456789");
+        Contact bob = new Contact("Bob", "31415926");
+        Contact alice = new Contact("Alice", " 161800339");
+        Contact tom = new Contact("Tom", "11235813");
+        Contact jane = new Contact("Jane", "23571113");
+
+        mobilePhone.addNewContact(bob);
+        mobilePhone.addNewContact(alice);
+        mobilePhone.addNewContact(tom);
+        mobilePhone.addNewContact(jane);
+
+        mobilePhone.printContacts();
+
+    }
 }
