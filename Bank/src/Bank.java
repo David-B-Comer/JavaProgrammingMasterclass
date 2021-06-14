0043b1d2db55a7981ea7bb64a4e080470f681b7b
David-B-Comer <davec2233@gmail.com>
2021-06-14 08:21:36 -0400

    added Bank listCustomer

--- Bank/src/Bank.java
+++ Bank/src/Bank.java
@@ -1,3 +1,4 @@
+import java.beans.Customizer;
 import java.util.ArrayList;
 import java.util.concurrent.BrokenBarrierException;
 
@@ -36,14 +37,13 @@ public class Bank {
 
     public boolean addCustomerTransaction(String branchName, String customerName, double transaction) {
 
-        Branch branch = new findBranch(branchName);
+        Branch branch = findBranch(branchName);
 
         if (branch != null) {
             return branch.addCustomerTransaction(customerName, transaction);
         }
         return false;
     }
-
 
     public Branch findBranch(String branchName) {
 
@@ -56,7 +56,32 @@ public class Bank {
         return null;
     }
 
+    public boolean listCustomer(String branchName, boolean printTransactions) {
 
+        Branch branch = findBranch(branchName);
 
+        if (branch != null) {
+            System.out.println("Customer details for branch " + branch.getName());
+
+            ArrayList<Customer> branchCustomers = branch.getCustomers();
+
+            for (int i = 0; i < branchCustomers.size(); i++) {
+                Customer branchCustomer = branchCustomers.get(i);
+                System.out.println("Customer: " + branchCustomer.getName() + "[" + (i + 1) + "]");
+
+                if (printTransactions) {
+                    System.out.println("Transactions");
+
+                    ArrayList<Double> transactions = branchCustomer.getTransactions();
+
+                    for (int j = 0; j < transactions.size(); j++) {
+                        System.out.println("[" + (j + 1) + "]  Amount "  + transactions.get(j));
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
 }
