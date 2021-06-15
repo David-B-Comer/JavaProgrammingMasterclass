d9ee5e3b65333b00defec8014847f13c6ef99e91
David-B-Comer <davec2233@gmail.com>
2021-06-15 15:30:51 -0400

    added Bank Customer constructor

--- Bank/src/Customer.java
+++ Bank/src/Customer.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    private ArrayList<Double> transactions;
+
+    public Customer(String name, double initialTransaction) {
+        this.name = name;
+        this.transactions = new ArrayList<Double>;
+    }
 }
