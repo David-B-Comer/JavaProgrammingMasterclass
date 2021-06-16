31e16f39a01c703d13c69c8dffec9f68e8d35872
David-B-Comer <davec2233@gmail.com>
2021-06-16 17:30:33 -0400

    added LinkedListCustomer LinkedListMain method calls

--- LinkedList/src/LinkedListMain.java
+++ LinkedList/src/LinkedListMain.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class LinkedListMain {
+
+    public static void main(String[] args) {
+
+        LinkedListCustomer customer = new LinkedListCustomer("Dave", 54.96);
+        LinkedListCustomer anotherCustomer;
+        anotherCustomer = customer;
+        anotherCustomer.setBalance(12.18);
+        System.out.println("Balance for customer " + customer.getName() + " is " + customer.getBalance());
+
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        
+        intList.add(1);
+        intList.add(3);
+        intList.add(4);
+
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " +intList.get(i));
+        }
+
+        intList.add(1, 2);
+
+        for (int i = 0; i < intList.size(); i++) {
+            System.out.println(i + ": " +intList.get(i));
+        }
+    }
 }
