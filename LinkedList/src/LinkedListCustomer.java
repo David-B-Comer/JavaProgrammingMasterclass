5d8016e2963166aafa2f94c7017a1d4fd9b3c75d
David-B-Comer <davec2233@gmail.com>
2021-06-16 17:12:13 -0400

    added LinkedListCustomer constructor

--- LinkedList/src/LinkedListCustomer.java
+++ LinkedList/src/LinkedListCustomer.java
@@ -1,2 +1,10 @@
 public class LinkedListCustomer {
+
+    private String name;
+    private double balance;
+
+    public LinkedListCustomer(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
 }
