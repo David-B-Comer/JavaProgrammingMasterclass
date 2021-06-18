e0dcb57112a71f8b14aecfe1b85d0ac9b015aea7
David-B-Comer <davec2233@gmail.com>
2021-06-17 21:20:48 -0400

    added LinkedListDemo placesToVisit

--- LinkedList/src/LinkedListDemo.java
+++ LinkedList/src/LinkedListDemo.java
@@ -1,2 +1,17 @@
+import java.util.LinkedList;
+
 public class LinkedListDemo {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> placesToVisit = new LinkedList<String>();
+        placesToVisit.add("Sydney");
+        placesToVisit.add("Melbourne");
+        placesToVisit.add("Brisbane");
+        placesToVisit.add("Perth");
+        placesToVisit.add("Canberra");
+        placesToVisit.add("Adelaide");
+        placesToVisit.add("Darwin");
+
+    }
 }
