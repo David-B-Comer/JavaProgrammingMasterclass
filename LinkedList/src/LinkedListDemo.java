030e10ceb4667cf3f3380490a8cb1a43cb7c8e13
David-B-Comer <davec2233@gmail.com>
2021-06-17 21:45:47 -0400

    added LinkedListDemo addInOrder method calls

--- LinkedList/src/LinkedListDemo.java
+++ LinkedList/src/LinkedListDemo.java
@@ -7,22 +7,23 @@ public class LinkedListDemo {
     public static void main(String[] args) {
 
         LinkedList<String> placesToVisit = new LinkedList<String>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
+
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
 
         printList(placesToVisit);
 
-        placesToVisit.add(1, "Alice Springs");
+        addInOrder(placesToVisit, "Alice Springs");
+        addInOrder(placesToVisit, "Darwin");
+
         printList(placesToVisit);
 
 
-        placesToVisit.remove(4);
-        printList(placesToVisit);
 
 
     }
