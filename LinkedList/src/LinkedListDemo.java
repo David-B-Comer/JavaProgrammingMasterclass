53c699d07dd6d25d62fe44dc10a2fb4a076eb07f
David-B-Comer <davec2233@gmail.com>
2021-06-18 20:19:48 -0400

    added LinkedListDemo visit

--- LinkedList/src/LinkedListDemo.java
+++ LinkedList/src/LinkedListDemo.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class LinkedListDemo {
 
@@ -61,6 +62,50 @@ public class LinkedListDemo {
         stringListIterator.add(newCity);
 
         return true;
+    }
+
+    private static void visit(LinkedList cities) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        ListIterator<String> listIterator = cities.listIterator();
+
+        if (cities.isEmpty()) {
+            System.out.println("No cities in the itinerary");
+            return;
+        } else {
+            System.out.println("Now visiting " + listIterator.next());
+            printMenu();
+        }
+
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("Holiday (Vacation) over");
+                    quit = true;
+                    break;
+                case 1:
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now visiting " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the list");
+                    }
+                    break;
+                case 2:
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now visiting " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the start of the list");
+                    }
+                    break;
+                case 3:
+                    printMenu();
+                    break;
+            }
+        }
     }
 
 }
