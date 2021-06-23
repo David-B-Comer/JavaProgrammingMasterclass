fc77c81c89fc8856cf3127621222791ec042e74f
David-B-Comer <davec2233@gmail.com>
2021-06-23 16:05:37 -0400

    added Telephone DeskPhone constructor

--- Gearbox/src/DeskPhone.java
+++ Gearbox/src/DeskPhone.java
@@ -3,6 +3,10 @@ public class DeskPhone implements Telephone{
     private int myNumber;
     private boolean isRinging;
 
+    public DeskPhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
     @Override
     public void powerOn() {
         System.out.println("No action taken, desk phone does not have a power button");
--- Gearbox/src/TelephoneMain.java
+++ Gearbox/src/TelephoneMain.java
@@ -1,2 +1,8 @@
 public class TelephoneMain {
+
+    public static void main(String[] args) {
+
+        Telephone telephone;
+        telephone = new DeskPhone(123456);
+    }
 }
