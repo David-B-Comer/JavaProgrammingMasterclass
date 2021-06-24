db94931a0fe7c47c9614b453b1c0ad953333da51
David-B-Comer <davec2233@gmail.com>
2021-06-24 18:10:32 -0400

    added Telephone CellPhone dial

--- Gearbox/src/CellPhone.java
+++ Gearbox/src/CellPhone.java
@@ -11,7 +11,20 @@ public class CellPhone implements Telephone {
 
     @Override
     public void powerOn() {
+
         isOn = true;
         System.out.println("Cell phone powered up");
     }
+
+    @Override
+    public void dial(int phoneNumber) {
+
+        if (isOn) {
+            System.out.println("Now ringing " + phoneNumber + " on mobile phone");
+        } else {
+            System.out.println("Phone is switched off");
+        }
+    }
+
+
 }
