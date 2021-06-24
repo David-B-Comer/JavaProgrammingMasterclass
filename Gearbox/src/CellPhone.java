9f3ed2e4ed52e2a5f789ebda24232f41e390e483
David-B-Comer <davec2233@gmail.com>
2021-06-24 18:07:11 -0400

    added Telephone CellPhone constructor

--- Gearbox/src/CellPhone.java
+++ Gearbox/src/CellPhone.java
@@ -1,2 +1,11 @@
-public class CellPhone {
+public class CellPhone implements Telephone {
+
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn = false;
+
+    public CellPhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
 }
