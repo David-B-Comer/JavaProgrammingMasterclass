e4e79338264d3a767237437298f8d72c0d874287
David-B-Comer <davec2233@gmail.com>
2021-06-27 17:33:20 -0400

    added ISaveable Monster constructor

--- ISaveable/src/Monster.java
+++ ISaveable/src/Monster.java
@@ -1,2 +1,13 @@
 public class Monster {
+
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
 }
