0c7f4870f307bafb4965d36a167eef81ec318f70
David-B-Comer <davec2233@gmail.com>
2021-06-28 17:37:45 -0400

    added Gearbox constructor

--- InnerClasses/src/Gearbox.java
+++ InnerClasses/src/Gearbox.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Gearbox {
+
+    private ArrayList<Gear> gears;
+    private int maxGears;
+    private int gearNumber = 0;
+
+    public Gearbox(int maxGears) {
+        this.maxGears = maxGears;
+        this.gears = new ArrayList<Gear>();
+        Gear neutral = new Gear(0, 0.0);
+        this.gears.add(neutral);
+    }
+
 }
