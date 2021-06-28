1b6203ba2c94ea491ec0f1d9b30377967c137a4a
David-B-Comer <davec2233@gmail.com>
2021-06-28 17:39:38 -0400

    added Gearbox Gear constructor

--- InnerClasses/src/Gearbox.java
+++ InnerClasses/src/Gearbox.java
@@ -15,6 +15,13 @@ public class Gearbox {
 
     public class Gear {
 
+        private int gearNumber;
+        private double ratio;
+
+        public Gear(int gearNumber, double ratio) {
+            this.gearNumber = gearNumber;
+            this.ratio = ratio;
+        }
 
     }
 }
