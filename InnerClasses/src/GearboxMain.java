f2dc0938a282f60b83dd69d74c31fd9363318ad2
David-B-Comer <davec2233@gmail.com>
2021-06-29 16:49:48 -0400

    added GearboxMain method calls

--- InnerClasses/src/GearboxMain.java
+++ InnerClasses/src/GearboxMain.java
@@ -1,2 +1,9 @@
 public class GearboxMain {
+
+    public static void main(String[] args) {
+
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+        System.out.println(first.driveSpeed(1000));
+    }
 }
