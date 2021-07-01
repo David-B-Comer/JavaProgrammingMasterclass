afc3507963febafc617a6c2af2d01cee42b05383
David-B-Comer <davec2233@gmail.com>
2021-06-30 20:39:59 -0400

    added Gearbox method calls

--- InnerClasses/src/GearboxMain.java
+++ InnerClasses/src/GearboxMain.java
@@ -3,9 +3,6 @@ public class GearboxMain {
     public static void main(String[] args) {
 
         Gearbox mcLaren = new Gearbox(6);
-        mcLaren.addGear(1, 5.3);
-        mcLaren.addGear(2, 10.6);
-        mcLaren.addGear(3, 15.9);
         mcLaren.operateClutch(true);
         mcLaren.changeGear(1);
         mcLaren.operateClutch(false);
