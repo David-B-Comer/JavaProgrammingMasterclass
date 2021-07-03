1387cac2de48ad936288ba6cbb4a11c8b4f6bd7b
David-B-Comer <davec2233@gmail.com>
2021-07-02 22:48:39 -0400

    added ButtonMain ClickListener

--- InnerClasses/src/ButtonMain.java
+++ InnerClasses/src/ButtonMain.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class ButtonMain {
+
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+    public static void main(String[] args) {
+
+        class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
+        }
+
+    }
 }
