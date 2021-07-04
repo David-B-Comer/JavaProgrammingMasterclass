2cde029c385492cc0ee2528348643a16e0c010ef
David-B-Comer <davec2233@gmail.com>
2021-07-04 10:28:26 -0400

    added Album2 constructor

--- Playlist2/src/Album2.java
+++ Playlist2/src/Album2.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Album2 {
+
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album2(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<Song>();
+    }
 }
