d4594209b4d822d43a2f397c7ef31a5504d781a6
David-B-Comer <davec2233@gmail.com>
2021-07-05 11:52:50 -0400

    added Song2 constructor

--- Playlist2/src/Song2.java
+++ Playlist2/src/Song2.java
@@ -1,2 +1,10 @@
 public class Song2 {
+
+    private String title;
+    private double duration;
+
+    public Song2(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
 }
