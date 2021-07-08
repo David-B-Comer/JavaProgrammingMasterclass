054ca15d091b17090d511b31f6b49c07dc7fda8f
David-B-Comer <davec2233@gmail.com>
2021-07-08 12:31:02 -0400

    added Animals Dogs eat

--- Abstraction/src/Dogs.java
+++ Abstraction/src/Dogs.java
@@ -3,4 +3,10 @@ public class Dogs extends Animals{
     public Dogs(String name) {
         super(name);
     }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is eating");
+    }
+
 }
