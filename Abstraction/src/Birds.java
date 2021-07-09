05ede8729b4a2b3482fedd5f1cb16d2d0f878479
David-B-Comer <davec2233@gmail.com>
2021-07-09 08:10:21 -0700

    added Animals Birds eat

--- Abstraction/src/Birds.java
+++ Abstraction/src/Birds.java
@@ -3,4 +3,9 @@ public class Birds extends Animals{
     public Birds(String name) {
         super(name);
     }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is pecking");
+    }
 }
