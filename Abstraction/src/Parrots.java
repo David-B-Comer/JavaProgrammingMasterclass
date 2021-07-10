695206882949b3f0080383315e1316a720c94bec
David-B-Comer <davec2233@gmail.com>
2021-07-10 09:23:06 -0700

    added Animals Parrots fly

--- Abstraction/src/Parrots.java
+++ Abstraction/src/Parrots.java
@@ -3,4 +3,10 @@ public class Parrots extends Birds {
     public Parrots(String name) {
         super(name);
     }
+
+    @Override
+    public void fly() {
+        System.out.println("Flitting from branch to branch");
+    }
+
 }
