c23cdd3b87d85569c3f430aed1d3cae78baaab80
David-B-Comer <davec2233@gmail.com>
2021-07-11 07:42:40 -0700

    added Animals Penguins fly

--- Abstraction/src/Penguins.java
+++ Abstraction/src/Penguins.java
@@ -3,4 +3,10 @@ public class Penguins extends Birds{
     public Penguins(String name) {
         super(name);
     }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm not very good at that, can I go for a swim instead?");
+    }
+
 }
