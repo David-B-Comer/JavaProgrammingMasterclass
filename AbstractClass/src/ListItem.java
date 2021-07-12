cdd6025269f0a5fa7e6b2fc61c89052914efb4be
David-B-Comer <davec2233@gmail.com>
2021-07-12 08:26:54 -0700

    added ListItem constructor

--- AbstractClass/src/ListItem.java
+++ AbstractClass/src/ListItem.java
@@ -1,2 +1,10 @@
-public class ListItem {
+public abstract class ListItem {
+
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
 }
