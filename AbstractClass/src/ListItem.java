71490ff73c1423ab54ad2bbdba14ec5cf13780f4
David-B-Comer <davec2233@gmail.com>
2021-07-14 17:41:15 -0400

    added ListItem Node compareTo

--- AbstractClass/src/ListItem.java
+++ AbstractClass/src/ListItem.java
@@ -24,7 +24,7 @@ public abstract class ListItem {
 
     abstract ListItem setPrevious(ListItem item);
 
-    abstract ListItem compareTo(ListItem item);
+    abstract int compareTo(ListItem item);
 
 
 }
--- AbstractClass/src/Node.java
+++ AbstractClass/src/Node.java
@@ -25,4 +25,15 @@ public class Node extends ListItem{
         this.leftLink = item;
         return leftLink;
     }
+
+    @Override
+    int compareTo(ListItem item) {
+
+        if (item != null) {
+            return ((String)super.getValue()).compareTo((String)item.getValue());
+        } else {
+            return -1;
+        }
+    }
+
 }
