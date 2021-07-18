421140ce6a1a33f7ab4e21811f7d9ea70417012e
David-B-Comer <davec2233@gmail.com>
2021-07-18 18:18:25 -0400

    added Generics items

--- Generics/src/Generics.java
+++ Generics/src/Generics.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Generics {
+
+    public static void main(String[] args) {
+
+        ArrayList items = new ArrayList();
+
+        items.add(1);
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+    }
+
 }
