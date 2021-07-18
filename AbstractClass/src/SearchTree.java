a50e64d06163c35de0f58f432e3ed3d7997e0943
David-B-Comer <davec2233@gmail.com>
2021-07-17 21:40:37 -0400

    added ListItem SearchTree addItem

--- AbstractClass/src/SearchTree.java
+++ AbstractClass/src/SearchTree.java
@@ -10,4 +10,42 @@ public class SearchTree implements NodeList {
     public ListItem getRoot() {
         return root;
     }
+
+    @Override
+    public boolean addItem(ListItem newItem) {
+
+        if (this.root == null) {
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+
+            if (comparison < 0) {
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    currentItem.setNext(newItem).setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem).setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem).setPrevious(newItem);
+                } else {
+                    newItem.setNext(this.root).setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            } else {
+                System.out.println(newItem.getValue() + " is already present, not added.");
+                return false;
+            }
+        }
+        return false;
+    }
+
 }
