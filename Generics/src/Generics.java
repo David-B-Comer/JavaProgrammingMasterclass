3423aee010c5fe87f327e35ed239116a39fa5368
David-B-Comer <davec2233@gmail.com>
2021-07-18 18:24:12 -0400

    added Generics printDoubled

--- Generics/src/Generics.java
+++ Generics/src/Generics.java
@@ -11,6 +11,15 @@ public class Generics {
         items.add(3);
         items.add(4);
         items.add(5);
+
+        printDoubled(items);
+    }
+
+    private static void printDoubled(ArrayList n) {
+
+        for (Object i : n) {
+            System.out.println((Integer) i * 2);
+        }
     }
 
 }
