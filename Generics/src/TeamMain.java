a09215b26469bf5d8cb65d5269b57fdaceb50b57
David-B-Comer <davec2233@gmail.com>
2021-07-23 14:19:54 -0400

    added Generics Team baseballTeam

--- Generics/src/TeamMain.java
+++ Generics/src/TeamMain.java
@@ -6,11 +6,16 @@ public class TeamMain {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer beckham = new SoccerPlayer("Beckham");
 
-        Team adelaideCrows = new Team("Adelaide Crows");
+        Team<FootballPlayer> adelaideCrows = new Team<>("Adelaide Crows");
         adelaideCrows.addPlayer(joe);
-        adelaideCrows.addPlayer(pat);
-        adelaideCrows.addPlayer(beckham);
+//        adelaideCrows.addPlayer(pat);
+//        adelaideCrows.addPlayer(beckham);
 
         System.out.println(adelaideCrows.numPlayers());
+
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+
     }
 }
