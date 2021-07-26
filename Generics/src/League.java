69302592bfba50f054d1daf53eeacb564faa6161
David-B-Comer <davec2233@gmail.com>
2021-07-26 16:18:06 -0400

    added League constructor

--- Generics/src/League.java
+++ Generics/src/League.java
@@ -5,4 +5,8 @@ public class League<T extends Team> {
     public String name;
     private ArrayList<T> league = new ArrayList<T>();
 
+    public League(String name) {
+        this.name = name;
+    }
+
 }
