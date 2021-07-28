fe62c40496690c29707ed54d81c1e9cbe14d655b
David-B-Comer <davec2233@gmail.com>
2021-07-28 17:23:45 -0400

    added SeriesMain for loop factorial

--- Series/src/SeriesMain.java
+++ Series/src/SeriesMain.java
@@ -6,5 +6,11 @@ public class SeriesMain {
             System.out.println(Series.nSum(i));
         }
 
+        System.out.println("**********************");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.factorial(i));
+        }
     }
+
 }
