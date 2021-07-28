772adeab6dfa8895ac5ea2fa8c86869644eab56b
David-B-Comer <davec2233@gmail.com>
2021-07-28 17:20:52 -0400

    added SeriesMain for loop

--- Series/src/SeriesMain.java
+++ Series/src/SeriesMain.java
@@ -2,6 +2,9 @@ public class SeriesMain {
 
     public static void main(String[] args) {
 
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(Series.nSum(i));
+        }
 
     }
 }
