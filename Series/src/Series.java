06ef10e11b3b5b33dcc908dca760b669078a0f22
David-B-Comer <davec2233@gmail.com>
2021-07-27 21:04:52 -0400

    added Series factorial

--- Series/src/Series.java
+++ Series/src/Series.java
@@ -3,6 +3,19 @@ public class Series {
     public static long nSum(int n) {
 
         return (n * (n + 1)) / 2;
-
     }
+
+    public static long factorial(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+        long fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
 }
