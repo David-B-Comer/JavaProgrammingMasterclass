77b7b0fa2b74e0301f1504e4031c319555da2d4a
David-B-Comer <davec2233@gmail.com>
2021-08-02 16:04:54 -0400

    added Scope var4

--- Scope/src/Scope.java
+++ Scope/src/Scope.java
@@ -2,12 +2,12 @@ public class Scope {
 
     public static void main(String[] args) {
 
-        String privateVar = "This is private to main()";
+        String var4 = "This is private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
 
         System.out.println("scopeInstance private var is " + scopeInstance.getVar1());
-        System.out.println(privateVar);
+        System.out.println(var4);
 
         scopeInstance.timesTwo();
         System.out.println("******************");
