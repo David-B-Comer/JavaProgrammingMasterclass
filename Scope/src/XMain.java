e331ff7c9ab85272c596f29ec6336d66935525ac
David-B-Comer <davec2233@gmail.com>
2021-08-03 09:24:47 -0400

    added Scope XMain method call

--- Scope/src/XMain.java
+++ Scope/src/XMain.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class XMain {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+
+        X x = new X(scanner.nextInt());
+        x.x();
+
+
+    }
 }
