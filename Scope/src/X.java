ad76d208f45e1784d60d552f451cfd2a52fa757d
David-B-Comer <davec2233@gmail.com>
2021-08-03 09:19:19 -0400

    added Scope X x

--- Scope/src/X.java
+++ Scope/src/X.java
@@ -5,4 +5,12 @@ public class X {
     public X(int x) {
         this.x = x;
     }
+
+    public void x() {
+
+        for (int x = 1; x < 13; x++) {
+            System.out.println(x + " times " + this.x + " equals " + x * this.x);
+        }
+    }
+
 }
