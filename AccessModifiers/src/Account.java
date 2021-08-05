8896072ac58da176886c136e306d27755e8ae4d0
David-B-Comer <davec2233@gmail.com>
2021-08-04 21:01:41 -0400

    added Account constructor

--- AccessModifiers/src/Account.java
+++ AccessModifiers/src/Account.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+
 public class Account {
+
+    public String accountName;
+    public int balance = 0;
+    public ArrayList<Integer> transactions;
+
+    public Account(String accountName) {
+        this.accountName = accountName;
+    }
 }
