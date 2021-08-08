db6fe90ba7ae775b949e7f74685e3d64493c27ad
David-B-Comer <davec2233@gmail.com>
2021-08-08 09:21:23 -0400

    added StaticTestMain thirdInstance

--- StaticTest/src/StaticTestMain.java
+++ StaticTest/src/StaticTestMain.java
@@ -8,6 +8,9 @@ public class StaticTestMain {
         StaticTest secondInstance = new StaticTest("2nd instance");
         System.out.println(secondInstance.getName() + " is instance number " + secondInstance.getNumInstances());
 
+        StaticTest thirdInstance = new StaticTest("3rd instance");
+        System.out.println(thirdInstance.getName() + " is instance number " + thirdInstance.getNumInstances());
+
     }
 
 }
