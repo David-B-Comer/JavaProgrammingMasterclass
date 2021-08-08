837ac6877d28d4e7d27c33c9b1d2d6261073c63c
David-B-Comer <davec2233@gmail.com>
2021-08-08 09:14:18 -0400

    added StaticTestMain firstInstance

--- StaticTest/src/StaticTestMain.java
+++ StaticTest/src/StaticTestMain.java
@@ -1,2 +1,9 @@
 public class StaticTestMain {
+
+    public static void main(String[] args) {
+
+        StaticTest firstInstance = new StaticTest("1st instance");
+        System.out.println(firstInstance.getName() + " is instance number " + firstInstance.getNumInstances());
+    }
+
 }
