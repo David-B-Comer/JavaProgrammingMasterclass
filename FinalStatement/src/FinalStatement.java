ac4270e4223bf7d8bcef4a9dc80c167d6b728bf7
David-B-Comer <davec2233@gmail.com>
2021-08-10 17:17:56 -0400

    added FinalStatement name

--- FinalStatement/src/FinalStatement.java
+++ FinalStatement/src/FinalStatement.java
@@ -2,9 +2,12 @@ public class FinalStatement {
 
     private static int classCounter = 0;
     public final int instanceNumber;
+    private final String name;
 
-    public FinalStatement() {
-        instanceNumber = 1;
+    public FinalStatement(String name) {
+        this.name = name;
+        classCounter++;
+        instanceNumber = classCounter;
     }
 
 }
