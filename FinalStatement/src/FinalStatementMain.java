267bd8af04ce12c740ea672c952dabdfb000295a
David-B-Comer <davec2233@gmail.com>
2021-08-10 17:23:00 -0400

    added FinalStatementMain method calls

--- FinalStatement/src/FinalStatementMain.java
+++ FinalStatement/src/FinalStatementMain.java
@@ -1,2 +1,13 @@
 public class FinalStatementMain {
+
+    public static void main(String[] args) {
+
+        FinalStatement one = new FinalStatement("one");
+        FinalStatement two = new FinalStatement("two");
+        FinalStatement three = new FinalStatement("three");
+
+        System.out.println(one.getInstanceNumber());
+        System.out.println(two.getInstanceNumber());
+        System.out.println(three.getInstanceNumber());
+    }
 }
