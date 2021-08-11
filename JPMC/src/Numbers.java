5c55e79c76cbee6cb42c436c231cf8b32c16a3b6
David-B-Comer <davec2233@gmail.com>
2021-08-11 15:49:52 -0400

    added Numbers findDigits method calls

--- JPMC/src/Numbers.java
+++ JPMC/src/Numbers.java
@@ -47,4 +47,10 @@ public class Numbers {
         return ans;
     }
 
+    public static void main(String[] args) {
+
+        String s = "owoftnuoer";
+        System.out.println(findDigits(s));
+    }
+
 }
