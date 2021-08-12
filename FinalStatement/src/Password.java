414c16ca6e8df165369fb87b8546d4e758d677a8
David-B-Comer <davec2233@gmail.com>
2021-08-12 14:39:14 -0400

    added Password encryptDecrypt

--- FinalStatement/src/Password.java
+++ FinalStatement/src/Password.java
@@ -3,7 +3,11 @@ public class Password {
     public static final int key = 748576362;
     private final int encryptedPassword;
 
-    public Password(int encryptedPassword) {
-        this.encryptedPassword = encryptedPassword;
+    public Password(int password) {
+        this.encryptedPassword = encryptDecrypt(encryptedPassword);
+    }
+
+    public int encryptDecrypt(int password) {
+        return password ^ key;
     }
 }
