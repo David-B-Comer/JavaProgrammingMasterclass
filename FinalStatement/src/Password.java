974db7ddbe9471201118d92f445caeea05052dda
David-B-Comer <davec2233@gmail.com>
2021-08-12 14:35:52 -0400

    added Password constructor

--- FinalStatement/src/Password.java
+++ FinalStatement/src/Password.java
@@ -2,4 +2,8 @@ public class Password {
 
     public static final int key = 748576362;
     private final int encryptedPassword;
+
+    public Password(int encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
 }
