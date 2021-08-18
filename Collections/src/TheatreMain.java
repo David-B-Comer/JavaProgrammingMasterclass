48292a8f23a665bee3b15c57535dde2b654ee259
David-B-Comer <davec2233@gmail.com>
2021-08-18 19:20:40 -0400

    added TheatreMain reserveSeat

--- Collections/src/TheatreMain.java
+++ Collections/src/TheatreMain.java
@@ -4,5 +4,11 @@ public class TheatreMain {
 
         Theatre theatre = new Theatre("Olympian", 8, 12);
         theatre.getSeats();
+
+        if (theatre.reserveSeat("H11")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
     }
 }
