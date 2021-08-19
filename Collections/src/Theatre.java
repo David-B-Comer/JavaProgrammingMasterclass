2c4dbbdcaa10cd6f7f89608535367c8b4970ac04
David-B-Comer <davec2233@gmail.com>
2021-08-19 18:22:52 -0400

    added Theatre seats Collection LinkedList

--- Collections/src/Theatre.java
+++ Collections/src/Theatre.java
@@ -1,10 +1,10 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class Theatre {
 
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<Seat>();
+    private Collection<Seat> seats = new LinkedList<>();
 
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
