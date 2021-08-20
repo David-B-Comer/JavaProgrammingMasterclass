4f540f579c308b2e8a7b75dc13786e9fc5c724d5
David-B-Comer <davec2233@gmail.com>
2021-08-20 13:50:10 -0400

    added TheatreMain seatCopy

--- Collections/src/TheatreMain.java
+++ Collections/src/TheatreMain.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class TheatreMain {
@@ -5,7 +6,8 @@ public class TheatreMain {
     public static void main(String[] args) {
 
         Theatre theatre = new Theatre("Olympian", 8, 12);
-
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        printList(seatCopy);
     }
 
     public static void printList(List<Theatre.Seat> list) {
