b0b8221cae4517053de8ea0c486b0464664153fb
David-B-Comer <davec2233@gmail.com>
2021-08-20 13:48:32 -0400

    added TheatreMain printList

--- Collections/src/TheatreMain.java
+++ Collections/src/TheatreMain.java
@@ -1,19 +1,21 @@
+import java.util.List;
+
 public class TheatreMain {
 
     public static void main(String[] args) {
 
         Theatre theatre = new Theatre("Olympian", 8, 12);
-        theatre.getSeats();
 
-        if (theatre.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
-        if (theatre.reserveSeat("H11")) {
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
     }
+
+    public static void printList(List<Theatre.Seat> list) {
+
+        for (Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("==============================================================================");
+
+    }
+
 }
