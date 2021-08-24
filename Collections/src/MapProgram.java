576f652834d729ff61ddca35e9aad6940d5ee786
David-B-Comer <davec2233@gmail.com>
2021-08-24 11:44:44 -0400

    added MapProgram languages

--- Collections/src/MapProgram.java
+++ Collections/src/MapProgram.java
@@ -1,8 +1,11 @@
-
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapProgram {
 
     public static void main(String[] args) {
+
+        Map<String, String> languages = new HashMap<>();
 
     }
 }
