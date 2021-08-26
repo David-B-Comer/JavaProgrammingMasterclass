c1d57a45f140273624a7a5d651142d88fb1bb11b
David-B-Comer <davec2233@gmail.com>
2021-08-26 19:12:59 -0400

    added MapProgram languages replace Scala

--- Collections/src/MapProgram.java
+++ Collections/src/MapProgram.java
@@ -32,6 +32,7 @@ public class MapProgram {
         }
 
         System.out.println(languages.replace("Lisp", "a functional programming language with imperative features"));
+        System.out.println(languages.replace("Scala", "this will not be added"));
 
         for (String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
