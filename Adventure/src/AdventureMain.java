e737a4dedbb329994f19c253c453e15446f3fdfa
David-B-Comer <davec2233@gmail.com>
2021-08-28 15:36:38 -0400

    added AdventureMain locations 0

--- Adventure/src/AdventureMain.java
+++ Adventure/src/AdventureMain.java
@@ -9,5 +9,7 @@ public class AdventureMain {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
     }
 }
