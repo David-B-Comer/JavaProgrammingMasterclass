a9fbd1445a56bbccd3b44e3ddabdf14ddcda4c1c
David-B-Comer <davec2233@gmail.com>
2021-08-27 21:58:55 -0400

    added MapProgram languages replace Lisp

--- Collections/src/MapProgram.java
+++ Collections/src/MapProgram.java
@@ -31,8 +31,13 @@ public class MapProgram {
             System.out.println("Algol not removed, key/value pair not found");
         }
 
-        System.out.println(languages.replace("Lisp", "a functional programming language with imperative features"));
-        System.out.println(languages.replace("Scala", "this will not be added"));
+        if (languages.replace("Lisp", "this will not work", "a functional programming language with imperative features")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("Lisp was not replaced");
+        }
+
+        //System.out.println(languages.replace("Scala", "this will not be added"));
 
         for (String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
