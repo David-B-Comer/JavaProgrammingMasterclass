8a12b243c73950ea3985a9a355ddb2c97c9b289f
David-B-Comer <davec2233@gmail.com>
2021-08-28 15:34:55 -0400

    added AdventureMain locations

--- Adventure/src/AdventureMain.java
+++ Adventure/src/AdventureMain.java
@@ -1,6 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AdventureMain {
+
+    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
     public static void main(String[] args) {
 
