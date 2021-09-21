09cb3a5674cc3338040f4b9505ebb0fe4add7715
David-B-Comer <davec2233@gmail.com>
2021-09-20 21:43:22 -0400

    added adventure location 4

--- Adventure/src/AdventureMain.java
+++ Adventure/src/AdventureMain.java
@@ -14,5 +14,6 @@ public class AdventureMain {
         locations.put(1, new Location(1, "You are standing at the end of a road before a small bridge"));
         locations.put(2, new Location(2, "You are at the top of a hill"));
         locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
     }
 }
