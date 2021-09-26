2a7e9638b5a6dd8e87e1d8a521ef082a160b5fbb
David-B-Comer <davec2233@gmail.com>
2021-09-26 17:40:05 -0400

    added SolarSystem HeavenlyBody satellites

--- SolarSystem/src/HeavenlyBody.java
+++ SolarSystem/src/HeavenlyBody.java
@@ -1,5 +1,8 @@
+import java.util.Set;
+
 public final class HeavenlyBody {
 
     private final String name;
     private final double orbintalPeriod;
+    private final Set<HeavenlyBody> satellites;
 }
