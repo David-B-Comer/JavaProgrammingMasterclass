d9f0f71afa13a02f7a680bf0282266cbfc4e03de
David-B-Comer <davec2233@gmail.com>
2021-09-28 20:53:55 -0400

    added SolarSystem solarSystem

--- SolarSystem/src/SolarSystem.java
+++ SolarSystem/src/SolarSystem.java
@@ -1,6 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SolarSystem {
 
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+
     public static void main(String[] args) {
+
 
     }
 }
