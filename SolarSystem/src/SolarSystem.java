7f788e7078fb6d6e6994ec351d53972d13a65e58
David-B-Comer <davec2233@gmail.com>
2021-09-28 20:54:40 -0400

    added SolarSystem planets

--- SolarSystem/src/SolarSystem.java
+++ SolarSystem/src/SolarSystem.java
@@ -1,9 +1,12 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class SolarSystem {
 
     private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
 
     public static void main(String[] args) {
 
