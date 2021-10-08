da0764f151f98c931b0d6941663ea27526471ed4
David-B-Comer <davec2233@gmail.com>
2021-10-08 17:11:13 -0400

    added SolarSystem override hashCode

--- SolarSystem/src/HeavenlyBody.java
+++ SolarSystem/src/HeavenlyBody.java
@@ -46,4 +46,11 @@ public final class HeavenlyBody {
 
         return this.name.equals(objName);
     }
+
+    @Override
+    public int hashCode() {
+        System.out.println("hashcode called");
+        return this.name.hashCode() + 57;
+    }
+
 }
