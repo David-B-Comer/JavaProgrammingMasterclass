426a255ad16b66ec78953673dc385f84684228e6
David-B-Comer <davec2233@gmail.com>
2021-10-11 11:42:07 -0400

    added DogMain rover2

--- SolarSystem/src/DogMain.java
+++ SolarSystem/src/DogMain.java
@@ -3,7 +3,7 @@ public class DogMain {
     public static void main(String[] args) {
 
         Labrador rover = new Labrador("Rover");
-
+        DogEquals rover2 = new DogEquals("Rover");
     }
 
 }
