c3cb15cd1fa426be847bc87e071a752f5bf4744f
David-B-Comer <davec2233@gmail.com>
2021-10-10 22:58:59 -0400

    added Labrador equals

--- SolarSystem/src/Labrador.java
+++ SolarSystem/src/Labrador.java
@@ -3,4 +3,20 @@ public class Labrador extends DogEquals{
     public Labrador(String name) {
         super(name);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj instanceof Labrador) {
+
+            String objName = ((Labrador) obj).getName();
+
+            return this.getName().equals(objName);
+        }
+        return false;
+    }
+
 }
