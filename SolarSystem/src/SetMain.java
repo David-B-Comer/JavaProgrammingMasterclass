0dc7e9a4b8c5eb00332350a77e883ecc0c5420cc
David-B-Comer <davec2233@gmail.com>
2021-10-12 20:32:02 -0400

    added SetMain squares

--- SolarSystem/src/SetMain.java
+++ SolarSystem/src/SetMain.java
@@ -1,2 +1,11 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetMain {
+
+    public static void main(String[] args) {
+
+        Set<Integer> squares = new HashSet<>();
+    }
+
 }
